0dc4632ccea37a8280174aed92a22079bfaa8561
9gr <67440879+FunctionalMetatable@users.noreply.github.com>
2021-03-19 18:28:22 +0000

    Update bookmarklet.js

--- autofill/bookmarklet.js
+++ autofill/bookmarklet.js
@@ -5,27 +5,27 @@ void(async () => { // Make sure there is no result so the page isn't overwritten
     let messages = Object.values(json);
     (menuItems => {
         let d = document.createElement("div"); // make the menu div
-        d.setAttribute("data-easrng-menu", "1"); // let the CSS see it exists without clashing with on-page styles
+        d.setAttribute("data-autofill-menu", "1"); // let the CSS see it exists without clashing with on-page styles
         let s = document.createElement("style"); // make a style tag
-        s.textContent = "[data-easrng-menubutton]:not(:root) { outline: none; margin: 0px; border: none; " + // add the CSS
+        s.textContent = "[data-autofill-menubutton]:not(:root) { outline: none; margin: 0px; border: none; " + // add the CSS
                 "border-radius: 0px; background: transparent; color: inherit; font: inherit; text" +
                 "-align: left; padding: 5px 10px 5px 20px; display: block; user-select: none; max" +
                 "-width: 300px; overflow: visible; height: fit-content; transition: none 0s ease " +
-                "0s !important; }[data-easrng-menubutton]:not(:root):hover, [data-easrng-menubutt" +
+                "0s !important; }[data-autofill-menubutton]:not(:root):hover, [data-autofill-menubutt" +
                 "on]:not(:root):focus { border: none; border-radius: 0px; background: rgb(200, 20" +
-                "0, 201); color: inherit; font: inherit; }[data-easrng-menubutton]:not(:root):dis" +
+                "0, 201); color: inherit; font: inherit; }[data-autofill-menubutton]:not(:root):dis" +
                 "abled { color: rgb(114, 119, 124); background: transparent; cursor: default; poi" +
-                "nter-events: none; }[data-easrng-menu]:not(:root) { line-height: 1.45; border: 1" +
+                "nter-events: none; }[data-autofill-menu]:not(:root) { line-height: 1.45; border: 1" +
                 "px solid rgb(218, 220, 224); position: fixed; top: 0px; left: 0px; z-index: 2147" +
                 "483647; min-width: 135px; background: rgb(255, 255, 255); padding: 3px 0px; disp" +
                 "lay: flex; flex-direction: column; text-align: left; color: rgb(0, 0, 0); font-f" +
                 "amily: system-ui, sans-serif; font-size: 13px; box-shadow: rgba(0, 0, 0, 0.557) " +
-                "3px 3px 2px -2px; }[data-easrng-blockbg] { position: fixed; top: 0px; left: 0px;" +
+                "3px 3px 2px -2px; }[data-autofill-blockbg] { position: fixed; top: 0px; left: 0px;" +
                 " width: 100%; height: 100%; z-index: 2147483647; }";
-        let c = _ => [...document.querySelectorAll("[data-easrng-menu],[data-easrng-blockbg]")].map(e => e.remove()); // this is a function that closes the menu
+        let c = _ => [...document.querySelectorAll("[data-autofill-menu],[data-autofill-blockbg]")].map(e => e.remove()); // this is a function that closes the menu
         d.append(...menuItems.map(e => { // this part turns the menu items into button tags
             let b = document.createElement("button"); // first we make the button
-            b.setAttribute("data-easrng-menubutton", "1"); // let the CSS see it exists without clashing with on-page styles
+            b.setAttribute("data-autofill-menubutton", "1"); // let the CSS see it exists without clashing with on-page styles
             b.textContent = e.text; // make it have the right text
             b.onclick = (a) => { // when it is clicked
                 a.preventDefault(); // don't reload the page
@@ -39,7 +39,7 @@ void(async () => { // Make sure there is no result so the page isn't overwritten
         }), s);
         d.style.margin = "10px" // make the menu not be so close to the edge
         let b = document.createElement("div"); // make a div that covers the page so we can close the menu by clicking behind it
-        b.setAttribute("data-easrng-blockbg", "1"); // let the CSS see it exists without clashing with on-page styles
+        b.setAttribute("data-autofill-blockbg", "1"); // let the CSS see it exists without clashing with on-page styles
         b.onclick = c; // when it's clicked, close the menu
         document
             .body
